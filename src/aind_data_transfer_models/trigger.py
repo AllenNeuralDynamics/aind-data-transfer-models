0c7fd354418de0e2b36334408425fe936cad9738
Alessio Buccino <alejoe9187@gmail.com>
2024-07-16 15:51:00 +0200

    feat: add TriggerConfigModel and tests

--- src/aind_data_transfer_models/trigger.py
+++ src/aind_data_transfer_models/trigger.py
@@ -6,7 +6,7 @@ from typing_extensions import Self
 
 from aind_data_schema_models.modalities import Modality
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 
 class ValidJobType(str, Enum):
@@ -25,76 +25,68 @@ class ValidJobType(str, Enum):
 class TriggerConfigModel(BaseModel):
     """Config to be parsed by the AIND Trigger Capsule."""
 
-    job_type: ValidJobType = (
-        Field(
-            description="The type of job to be triggered.",
-        ),
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    job_type: ValidJobType = Field(
+        description="The type of job to be triggered.",
     )
-    bucket: Optional[str] = (
-        Field(
-            description="The bucket where the data is stored.",
-            default=None,
-        ),
+    bucket: Optional[str] = Field(
+        description="The bucket where the data is stored.",
+        default=None,
     )
-    prefix: Optional[str] = (
-        Field(
-            description="The prefix where the data is stored.",
-            default=None,
-        ),
+    prefix: Optional[str] = Field(
+        description="The prefix where the data is stored.",
+        default=None,
     )
-    asset_name: Optional[str] = (
-        Field(
-            description="The name of the asset.",
-            default=None,
-        ),
+    asset_name: Optional[str] = Field(
+        description="The name of the asset.",
+        default=None,
     )
-    mount: Optional[str] = (
-        Field(
-            description="The mount point for the data.",
-            default=None,
-        ),
+
+    mount: Optional[str] = Field(
+        description="The mount point for the data.",
+        default=None,
     )
-    input_data_asset_id: Optional[Union[str, List[str]]] = (
-        Field(
-            description=(
-                "The ID of the input data asset. To attach multiple "
-                "input data assets, use semicolon as a delimiter."
-            ),
-            default=None,
+    input_data_asset_id: Optional[Union[str, List[str]]] = Field(
+        description=(
+            "The ID of the input data asset. To attach multiple "
+            "input data assets, use semicolon as a delimiter."
         ),
+        default=None,
     )
-    input_data_mount: Optional[Union[str, List[str]]] = (
-        Field(
-            description=(
-                "The mount point for the input data. If None, the input data "
-                "mount will be the same as the data asset name. For multiple "
-                "input data assets, use semicolon as a delimiter to specify "
-                "the mount path for each input data asset."
-            ),
-            default=None,
+
+    input_data_mount: Optional[Union[str, List[str]]] = Field(
+        description=(
+            "The mount point for the input data. If None, the input data "
+            "mount will be the same as the data asset name. For multiple "
+            "input data assets, use semicolon as a delimiter to specify "
+            "the mount path for each input data asset."
         ),
+        default=None,
     )
-    process_capsule_id: Optional[str] = (
-        Field(
-            description=(
-                "The ID of the process capsule. Ignored if job type is "
-                "run_generic_pipeline."
-            ),
-            default=None,
+
+    process_capsule_id: Optional[str] = Field(
+        description=(
+            "The ID of the process capsule. Ignored if job type is "
+            "run_generic_pipeline."
         ),
+        default=None,
     )
+
     capsule_version: Optional[str] = (
         Field(
             description="The version of the capsule.",
             default=None,
         ),
     )
+
     results_suffix: Optional[str] = (
         Field(
             description="The suffix to be added to the results.",
             default="processed",
         ),
     )
+
     input_data_asset_name: Optional[str] = Field(
         description=(
             "The name to use as stem for the captured asset. It is required "
@@ -126,7 +118,8 @@ class TriggerConfigModel(BaseModel):
     )
 
     @model_validator(mode="after")
-    def validate_trigger_config(self) -> Self: # noqa
+    def validate_trigger_config(self) -> Self:  # noqa
+        """Validate the trigger config."""
         # registration
         if self.bucket is not None and self.prefix is not None:
             if self.input_data_asset_id is not None:
@@ -147,26 +140,31 @@ class TriggerConfigModel(BaseModel):
             if self.process_capsule_id is None:
                 self.process_capsule_id = self.capsule_id
 
-        # input data asset ids
+        # input data asset ids, mounts, and names
         if self.input_data_asset_id is not None:
             if isinstance(self.input_data_asset_id, str):
                 self.input_data_asset_id = self.input_data_asset_id.split(";")
             if self.input_data_mount is not None:
                 self.input_data_mount = self.input_data_mount.split(";")
-                if isinstance(self.input_data_asset_id, list):
-                    if not isinstance(self.input_data_mount, list):
-                        raise ValueError(
-                            "input_data_mount should be a list if "
-                            "input_data_asset_id is a list."
-                        )
-                    if len(self.input_data_asset_id) != len(
-                        self.input_data_mount
-                    ):
-                        raise ValueError(
-                            "input_data_asset_id and input_data_mount should "
-                            "have the same length when multiple input data "
-                            "assets are attached."
-                        )
+            if isinstance(self.input_data_asset_id, list):
+                if not isinstance(self.input_data_mount, list):
+                    raise ValueError(
+                        "input_data_mount should be a list if "
+                        "input_data_asset_id is a list."
+                    )
+                if len(self.input_data_asset_id) != len(
+                    self.input_data_mount
+                ):
+                    raise ValueError(
+                        "input_data_asset_id and input_data_mount should "
+                        "have the same length when multiple input data "
+                        "assets are attached."
+                    )
+                if self.input_data_asset_name is None:
+                    raise ValueError(
+                        "input_data_asset_name is required when multiple "
+                        "input data assets are attached."
+                    )
 
         # generic pipeline
         if self.job_type == ValidJobType.RUN_GENERIC_PIPELINE:
